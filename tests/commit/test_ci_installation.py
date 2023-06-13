6e1ff833a1feafd332693530d5d8ac0fefbbac35
holl- <philipp@mholl.de>
2023-06-13 14:50:21 +0200

    Fixes

--- tests/commit/test_ci_installation.py
+++ tests/commit/test_ci_installation.py
@@ -1,16 +1,16 @@
 from unittest import TestCase
 
-import phi
+import unifyml
 
 
 class TestCIInstallation(TestCase):
 
     def test_detect_tf_torch_jax(self):
-        backends = phi.detect_backends()
+        backends = unifyml.detect_backends()
         names = [b.name for b in backends]
         self.assertIn('PyTorch', names)
         self.assertIn('Jax', names)
         self.assertIn('TensorFlow', names)
 
     def test_verify(self):
-        phi.verify()
+        unifyml.verify()
