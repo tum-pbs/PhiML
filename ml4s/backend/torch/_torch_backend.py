Fix automatic float conversion in gradient()

ml4s/backend/torch/_torch_backend.py
@@ -857,7 +857,7 @@ class TorchBackend(Backend):
 
     def _prepare_graph_inputs(self, args: tuple, wrt: Union[tuple, list]):
         args = [self.as_tensor(arg, True) if i in wrt else arg for i, arg in enumerate(args)]
-        args = [self.to_float(arg) if self.dtype(arg).kind == int else arg for arg in args]
+        args = [self.to_float(arg) if self.dtype(arg).kind == int and i in wrt else arg for i, arg in enumerate(args)]
         for i, arg in enumerate(args):
             if self.is_tensor(arg, True) and arg.requires_grad and not arg.is_leaf:
                 arg = torch.clone(arg).detach()