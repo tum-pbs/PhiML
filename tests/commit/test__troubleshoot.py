5b339aa6133d7c283a2a8173da39a5a8d896072d
holl- <philipp@mholl.de>
2023-07-20 13:34:04 +0200

    Add test__troubleshoot.py
    * Exclude exception handling blocks from coverage

---
+++ tests/commit/test__troubleshoot.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from ml4s._troubleshoot import assert_minimal_config, troubleshoot, count_tensors_in_memory, plot_solves
+from ml4s import math
+
+class TestTroubleshoot(TestCase):
+
+    def test_assert_minimal_config(self):
+        assert_minimal_config()
+
+    def test_troubleshoot(self):
+        troubleshoot()
+
+    def test_count_tensors_in_memory(self):
+        count_tensors_in_memory()
+
+    def test_plot_solves(self):
+        with plot_solves():
+            math.solve_linear(lambda x: 2 * x, math.tensor(1.), math.Solve(x0=0))
