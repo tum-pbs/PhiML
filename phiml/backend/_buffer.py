10a75ea0c99c8a3aae4cca994fc1e7437a2c0543
Philipp Holl <philipp.holl@tum.de>
2024-04-19 23:44:27 +0200

    Add register_buffer_deferred

--- phiml/backend/_buffer.py
+++ phiml/backend/_buffer.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Tuple, Callable, Any, Dict
+from typing import Sequence, Tuple, Callable, Any, Dict, Optional
 
 from ._backend import Backend, choose_backend, TensorType
 
@@ -49,7 +49,7 @@ def register_buffer(name: str, min_size: TensorType, default_size: int) -> int:
     Args:
         name: Buffer name.
         min_size: Required buffer size. This can be a placeholder / tracer.
-        default_size:
+        default_size: Size to use when no other information is available.
 
     Returns:
         Buffer size as `int`.
@@ -73,6 +73,42 @@ def register_buffer(name: str, min_size: TensorType, default_size: int) -> int:
     else:  # no config available, use default
         _CURRENT_TRACE[buffer_id] = default_size
         return default_size
+
+
+def register_buffer_deferred(name: str, dependent_tensors: Sequence[TensorType], default_size: int) -> Tuple[Optional[int], Callable]:
+    """
+    Register a buffer without having a placeholder for its required size yet.
+    The required size needs to be provided later
+
+    Args:
+        name: Buffer name.
+        dependent_tensors: All relevant tensors that can influence the required size of the buffer.
+        default_size: Size to use when no other information is available.
+
+    Returns:
+        size: Buffer size to use this time.
+        provide_size_fn: Function to call to provide the required size placeholder of the buffer
+    """
+    b = choose_backend(*dependent_tensors)
+    if all(b.is_available(t) for t in dependent_tensors):
+        return None, lambda s: None
+    if b.name in ['torch', 'tensorflow']:
+        return None, lambda s: None  # PyTorch & TF allow variable shapes during tracing. Jax doesn't
+    # --- determine id ---
+    i = 0
+    while f"{name}{i}" in _REQUIRED_SIZES:
+        i += 1
+    buffer_id = f"{name}{i}"
+    # --- Register buffer ---
+    def provide_size(min_size: TensorType):
+        _REQUIRED_SIZES[buffer_id] = min_size
+    if _SET_CONFIG:
+        assert buffer_id in _SET_CONFIG, f"Buffer {buffer_id} was not registered during previous traces"
+        _CURRENT_TRACE[buffer_id] = _SET_CONFIG[buffer_id]
+        return _SET_CONFIG[buffer_id], provide_size
+    else:  # no config available, use default
+        _CURRENT_TRACE[buffer_id] = default_size
+        return default_size, provide_size
 
 
 def wasted_memory(config: Dict[str, int], required: Dict[str, int]):
