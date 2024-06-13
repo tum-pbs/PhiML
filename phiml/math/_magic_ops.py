e897f9042a721eee4b730eb195de2439e3854aae
Philipp Holl <philipp.holl@tum.de>
2024-06-13 18:57:04 +0200

    Remove name-indexing in unstack()

--- phiml/math/_magic_ops.py
+++ phiml/math/_magic_ops.py
@@ -91,8 +91,7 @@ def unstack(value, dim: DimFilter) -> tuple:
                 assert isinstance(result, tuple), f"__unstack__ must return a tuple but got {type(result)}"
                 assert all([isinstance(item, Sliceable) for item in result]), f"__unstack__ must return a tuple of Sliceable objects but not all items were sliceable in {result}"
                 return result
-        dim_description = dims.item_names[0] if dims.item_names[0] is not None else range(dims.size)
-        return tuple([slice_(value, {dims.name: i}) for i in dim_description])
+        return tuple([slice_(value, {dims.name: i}) for i in range(dims.size)])
     else:  # multiple dimensions
         if hasattr(value, '__pack_dims__'):
             packed_dim = batch('_unstack')
