d6572f1f2319cc4b0d767ddf9a5cdb11c9d58dd6
Philipp Holl <philipp.holl@tum.de>
2025-02-12 23:07:09 +0100

    Fix jit compilation of sparse sum_equal_entries()

--- phiml/math/_sparse.py
+++ phiml/math/_sparse.py
@@ -1781,14 +1781,14 @@ def sum_equal_entries(matrix: Tensor, flatten_entries=True):
     if num_entries == instance(values).volume:
         matrix._can_contain_double_entries = False
         return matrix
-    b = matrix.default_backend
+    bv = matrix.default_backend
     if non_instance(values):
         batched_values = values.native([non_instance, 'sp_entries'])
-        values_nat = b.batched_bincount(u_ptr[None, :], weights=batched_values, bins=num_entries)
+        values_nat = bv.batched_bincount(u_ptr[None, :], weights=batched_values, bins=num_entries)
         values = wrap(values_nat, non_instance(values), instance('sp_entries'))
     else:
-        values = b.bincount(u_ptr, weights=values.native(), bins=num_entries)
-        values = reshaped_tensor(values, [instance('sp_entries')])
+        values = bv.bincount(u_ptr, weights=values.native(), bins=num_entries)
+        values = reshaped_tensor(values, [instance('sp_entries')], convert=False)
     idx_packed = b.unravel_index(u_idx, dims.sizes)
     indices = wrap(idx_packed, instance('sp_entries'), channel(matrix._indices))
     return SparseCoordinateTensor(indices, values, matrix._dense_shape, False, True, matrix._indices_constant, matrix._matrix_rank)
