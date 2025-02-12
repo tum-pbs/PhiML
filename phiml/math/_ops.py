bdcddd98fca353de0e734616feca0c7b81f57f18
Philipp Holl <pholl@europe.altair.com>
2025-02-12 19:52:48 +0100

    Fix scatter with instance dims treated as channel

--- phiml/math/_ops.py
+++ phiml/math/_ops.py
@@ -2981,11 +2981,11 @@ def scatter(base_grid: Union[Tensor, Shape],
     broadcast = broadcast_dims(base_grid, indices, values)
     def scatter_forward(base_grid: Tensor, indices: Tensor, values: Tensor, indexed_dims=indexed_dims):
         indexed_dims = base_grid.shape[indexed_dims] - broadcast
-        batches = values.shape.non_channel.non_instance & indices.shape.non_channel.non_instance
+        batches = (values.shape.non_channel.non_instance & indices.shape.non_channel.non_instance & (indices.shape.instance.only(base_grid.shape.instance))) - indexed_dims
         batches &= values.shape.only(treat_as_batch) & indices.shape.only(treat_as_batch)
         batches -= broadcast
         channels = (base_grid.shape - indexed_dims - batches - broadcast) & values.shape.channel
-        lists = indices.shape.instance & values.shape.instance
+        lists = (indices.shape.non_channel & values.shape.non_channel) - batches - broadcast - channels
         if values._is_tracer:
             if indices._is_tracer or base_grid._is_tracer:
                 raise NotImplementedError("scattering linear tracer into linear tracer not supported")
