b0fe601af5b50ef27d2e97d652c0f51b56deb8e9
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2025-05-06 01:04:02 +0200

    Fix small typo (#11)
    Co-authored-by: holl- <pholl@mpp.mpg.de>

--- phiml/math/_functional.py
+++ phiml/math/_functional.py
@@ -279,7 +279,7 @@ class JitFunction:
         if isinstance(self.f, GradientFunction) and key.backend.supports(Backend.jit_compile_grad):
             return self.grad_jit(*args, **kwargs)
         if not key.backend.supports(Backend.jit_compile):
-            warnings.warn(f"jit_copmile() not supported by {key.backend}. Running function '{f_name(self.f)}' as-is.", RuntimeWarning, stacklevel=2)
+            warnings.warn(f"jit_compile() not supported by {key.backend}. Running function '{f_name(self.f)}' as-is.", RuntimeWarning, stacklevel=2)
             return self.f(*args, **kwargs)
         # --- do we need to trace? ---
         if key in self.traces:
