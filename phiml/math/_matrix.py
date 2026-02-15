806de975505e7b58644d281e6c1f61b12e0bf19c
Philipp Holl <philipp.holl@tum.de>
2026-02-15 21:47:39 +0100

    Reduced linear solves
    Remove empty rows/cols from matrix in linear solve
    
    * Fix min_rank_deficiency during JIT
    * Fix JIT linear solve
    * Implement drop_rows_and_cols_from_system
    * Fix lin solve backprop with empty rows/cols
    * Implement empty rows/cols for sparse/dense matrices

--- phiml/math/_matrix.py
+++ phiml/math/_matrix.py
@@ -1,11 +1,17 @@
-from typing import Optional
+import warnings
+from typing import Sequence
 
-from ..backend import get_precision
-from ._shape import Shape, instance, EMPTY_SHAPE, dual
-from ._magic_ops import pack_dims, expand
-from ._tensors import Tensor, wrap
+import numpy as np
+from scipy.sparse.linalg import aslinearoperator
+
+from ..backend import get_precision, choose_backend
+from ._shape import Shape, instance, EMPTY_SHAPE, dual, channel, isize, DEBUG_CHECKS, non_batch, primal, batch
+from ._magic_ops import pack_dims, expand, concat, rename_dims
+from ._tensors import Tensor, wrap, reshaped_tensor
+from ._sparse import get_format, to_format, sparse_tensor, is_sparse, dense, native_matrix
 from ._lin_trace import LinTracer, dependent_src_dims
 from . import _ops as math
+from ..backend._dtype import INT64
 
 
 def min_rank_deficiency(matrix: Tensor) -> Tensor:
@@ -19,16 +25,22 @@ def min_rank_deficiency(matrix: Tensor) -> Tensor:
         tracer = matrix._prop.tracer
         if isinstance(tracer, LinTracer):
             if not tracer._fac.available:
-                return guaranteed_empty_rows(tracer)
+                return wrap(isize(guaranteed_empty_rows(matrix)))
+            fac = math.convert(tracer._fac, math.NUMPY)
             deficiency = wrap(0)
-            row_sums = math.sum_(tracer._fac, '_deps')
-            abs_row_sums = math.sum_(abs(tracer._fac), '_deps')
+            row_sums = math.sum_(fac, '_deps')
+            abs_row_sums = math.sum_(abs(fac), '_deps')
             eps = {16: 1e-2, 32: 1e-5, 64: 1e-10}[get_precision()]
-            if math.close(0, row_sums, rel_tolerance=0, abs_tolerance=eps * abs_row_sums, reduce=tracer._indices.shape):
-                deficiency += 1
+            is_balanced = math.close(0, row_sums, rel_tolerance=0, abs_tolerance=eps * abs_row_sums, reduce=tracer._indices.shape)
+            deficiency += is_balanced
             deficiency += math.sum_(abs_row_sums == 0, tracer._indices.shape)  # number of zero rows
             return deficiency
-    raise NotImplementedError(matrix)
+    else:
+        row_sums = math.sum_(matrix, dual)
+        abs_row_sums = math.sum_(abs(matrix), dual)
+        eps = {16: 1e-2, 32: 1e-5, 64: 1e-10}[get_precision()]
+        is_balanced = math.close(0, math.stop_gradient(row_sums), rel_tolerance=0, abs_tolerance=eps * abs_row_sums, reduce=non_batch)
+        return math.where(is_balanced, 1, 0)
 
 
 def guaranteed_empty_rows(matrix: Tensor) -> Tensor:
@@ -40,7 +52,12 @@ def guaranteed_empty_rows(matrix: Tensor) -> Tensor:
             fac_nz = tracer._fac != 0 if tracer._fac.available else tracer._fac_nz
             is_zero_row = ~math.any_(fac_nz, '_deps')
             return math.nonzero(is_zero_row, list_dim=instance('empties'), element_dims=None, list_dims=is_zero_row.shape)
-    raise NotImplementedError(matrix)
+    else:
+        if matrix.available:
+            is_zero_row = math.sum_(abs(matrix), dual) == 0
+            return math.nonzero(is_zero_row, list_dim=instance('empties'), element_dims=None, list_dims=is_zero_row.shape)
+        else:
+            return math.zeros(instance(empties=0), channel(vector=primal(matrix).name_list))
 
 
 def guaranteed_empty_cols(matrix: Tensor) -> Tensor:
@@ -59,7 +76,12 @@ def guaranteed_empty_cols(matrix: Tensor) -> Tensor:
             nz_indices = flat_indices[flat_nz]
             src_unused = math.scatter(expand(True, dependent_src_dims(tracer)), nz_indices, False)
             return math.nonzero(src_unused, list_dim=instance('empties'), element_dims=None, list_dims=src_unused.shape)
-    raise NotImplementedError(matrix)
+    else:
+        if matrix.available:
+            is_zero_col = math.sum_(abs(matrix), primal) == 0
+            return math.nonzero(is_zero_col, list_dim=instance('empties'), element_dims=None, list_dims=is_zero_col.shape)
+        else:
+            return math.zeros(instance(empties=0), channel(vector=primal(matrix).name_list))
 
 
 # def zero_rows(matrix: Tensor) -> Tensor[int]:
@@ -116,3 +138,67 @@ def matrix_rank(matrix: Tensor) -> Tensor:
         native = matrix.native([batch, primal, dual], force_expand=True)
         ranks_native = choose_backend(native).matrix_rank_dense(native)
         return reshaped_tensor(ranks_native, [batch(matrix)], convert=False)
+
+
+def drop_rows_and_cols_from_system(matrix: Tensor, primal_x_shape: Shape, empty_rows: Tensor, empty_cols: Tensor):
+    original_format = get_format(matrix)
+    rows = matrix.shape.primal
+    cols = matrix.shape.dual
+    primal_cols = primal_x_shape[cols.as_channel().name_list]
+    if original_format in {'csr', 'csc'}:
+        matrix = matrix.decompress()  # no cost if was compressed from coo
+    empty_rows = math.ravel_index(empty_rows[rows.name_list], matrix.shape)
+    empty_cols = math.ravel_index(empty_cols[primal_cols.name_list], matrix.shape)
+    matrix = pack_dims(matrix, rows, channel('rows'))  # respects order given by rows
+    matrix = pack_dims(matrix, cols, dual('cols'))
+    if DEBUG_CHECKS:
+        to_format(matrix, 'csr')  # check no out-of-bounds indices
+        assert isize(matrix.rows[empty_rows]._values) == 0  # no values in empty rows
+        assert isize(matrix[{dual: empty_cols}]._values) == 0  # no values in empty cols
+    # --- Remap matrix indices ---
+    get_old_row, get_new_row = index_map_remove(matrix.shape['rows'], empty_rows)
+    get_old_col, get_new_col = index_map_remove(matrix.shape['~cols'], empty_cols)
+    new_row = get_new_row[matrix.primal_indices()]
+    new_col = get_new_col[matrix.dual_indices()]
+    if DEBUG_CHECKS:
+        assert new_col.min >= 0  # if get_new_row is incorrect, we would get -1 entries
+        assert new_row.min >= 0
+    new_indices = concat([new_row, new_col], 'idx')
+    # --- Construct new matrix ---
+    values = matrix._values  # only indices shifted, values are unchanged
+    matrix = sparse_tensor(new_indices, values, matrix.shape - isize(empty_rows), can_contain_double_entries=original_format == 'coo', indices_sorted=original_format != 'coo', format=original_format)
+    # if DEBUG_CHECKS:
+    #     matrix.print('numpy', float_format='.2f')
+    #     matrix = dense(matrix).numpy(['rows', '~cols'])
+    #     nullity = matrix.shape[0] - np.linalg.matrix_rank(matrix)
+    #     inv = np.linalg.inv(matrix)
+    # --- Remap inputs/outputs ---
+    get_old_col_primal = rename_dims(get_old_col, dual, channel)
+    zeros_x = math.zeros(primal_cols)
+    get_old_col_exp = math.unravel_index(get_old_col.idx['~cols'].cols.dual.as_instance(), primal_cols)
+    zeros_y = math.zeros(rows)
+    get_old_row_exp = math.unravel_index(get_old_row.idx['rows'].rows.as_instance(), rows)
+
+    def contract_x(x: Tensor):
+        return math.gather(pack_dims(x, cols.as_batch().names, dual('cols')), get_old_col_primal, pref_index_dim='idx')
+
+    def contract_y(y: Tensor):
+        return math.gather(pack_dims(y, rows, channel('rows')), get_old_row, pref_index_dim='idx')
+
+    def expand_x(x: Tensor):
+        return math.scatter(zeros_x, get_old_col_exp, x.cols.as_instance())
+
+    def expand_y(y: Tensor):
+        return math.scatter(zeros_y, get_old_row_exp, y.rows.as_instance())
+
+    return matrix, contract_x, contract_y, expand_x, expand_y
+
+
+def index_map_remove(dim: Shape, to_remove: Tensor) -> Tensor:
+    indices_np = to_remove.numpy([instance])
+    keep = np.setdiff1d(np.arange(dim.size), indices_np)
+    reverse = np.full(dim.size, -1, dtype=int)
+    reverse[keep] = np.arange(keep.size)
+    keep = expand(wrap(keep, dim.without_sizes()), channel(idx=dim.name))
+    reverse = expand(wrap(reverse, dim.without_sizes()), channel(idx=dim.name))
+    return keep, reverse
