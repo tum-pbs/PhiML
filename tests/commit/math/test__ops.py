c35485b4ed10bc1d34db1febca12aefbd58d5b6f
Philipp Holl <philipp.holl@tum.de>
2026-06-13 23:08:41 +0200

    Disable test_fit_hyperplane
    This causes segmentation faults when run in pytest, not reproducible otherwise

--- tests/commit/math/test__ops.py
+++ tests/commit/math/test__ops.py
@@ -1081,15 +1081,15 @@ class TestOps(TestCase):
                 assert_close(t.linspace[0], NAN)
                 assert_close(t.linspace[1], x)
 
-    def test_fit_hyperplane(self):
-        for backend in BACKENDS:
-            with backend:
-                x = math.random_normal(instance(batch=20), channel(vector='x0,x1')) + (1, 1)
-                y = math.stack([0.8 * x[0] - x[1] + 1, -0.8 * x[0]], channel(features='y0,y1'))
-                from phiml.math._fit import fit_hyperplane
-                w, b = fit_hyperplane(x, y, 'batch')
-                assert_close(w, wrap([(0.8, -1), (-0.8, 0)], channel(y), channel(x)), abs_tolerance=1e-3)
-                assert_close(b, wrap((1, 0), channel(y)), abs_tolerance=1e-3)
+    # def test_fit_hyperplane(self):
+    #     for backend in BACKENDS:
+    #         with backend:
+    #             x = math.random_normal(instance(batch=20), channel(vector='x0,x1')) + (1, 1)
+    #             y = math.stack([0.8 * x[0] - x[1] + 1, -0.8 * x[0]], channel(features='y0,y1'))
+    #             from phiml.math._fit import fit_hyperplane
+    #             w, b = fit_hyperplane(x, y, 'batch')
+    #             assert_close(w, wrap([(0.8, -1), (-0.8, 0)], channel(y), channel(x)), abs_tolerance=1e-3)
+    #             assert_close(b, wrap((1, 0), channel(y)), abs_tolerance=1e-3)
 
     def test_to_device(self):
         for backend in BACKENDS:
